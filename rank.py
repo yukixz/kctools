92ff6d10dc474ab845fe37d2c4b69cdbf1a5b68a
Dazzy Ding <h@dazzyd.org>
2016-08-11 22:42:42 +0800

    rank: A little format

--- rank.py
+++ rank.py
@@ -6,7 +6,6 @@ from math import sqrt
 Il = [2966, 7966, 1553, 2139, 3299, 4131817, 1033183, 5855, 7803, 6443, 13,
       7157, 3791, 10, 9187, 9115, 1000, 1876163]
 # Maigc number is calculated from 'core.apis._APIBaseS_/I1'
-# `param2.s(param2.y(Il[Il[8 | param2.x() | 1]]))`
 MAGIC_13 = str(sqrt(13))
 MAGIC = [-1] * 10
 for i in range(10):
@@ -20,8 +19,9 @@ for i in range(10):
 def _l_(id):
     return int(str(MAGIC[id % 10])[0:2])
 
-# `rank_rate` is refactored from 'scene.record.models.RankData/RankData'
+# Magic R is extracted from 'scene.record.models.RankData/RankData'
+MAGIC_R = [ 8831, 1201, 1175, 555, 4569, 4732, 3779, 4568, 5695, 4619, 4912, 5669, 6569 ]
+# `real_rate` is refactored from 'scene.record.models.RankData/RankData'
 # We assume that it always is even divisible.
-def rank_rate(member_id, rank_no, obfs_rate):
-    MAGIC_R_NUMS = [ 8831, 1201, 1175, 555, 4569, 4732, 3779, 4568, 5695, 4619, 4912, 5669, 6569 ]
-    return obfs_rate / MAGIC_R_NUMS[rank_no % 13] / _l_(member_id) - 73 - 18
+def real_rate(login_member_id, rank_no, rank_rate):
+    return rank_rate / MAGIC_R[rank_no % 13] / _l_(login_member_id) - 73 - 18
